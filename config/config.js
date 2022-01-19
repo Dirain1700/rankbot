fd3960be7c61d03e9f04443bff3e28eb9e6c6c36
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-01-19 13:54:45 +0900

    Add config.js via upload

---
+++ config/config.js
@@ -0,0 +1,6 @@
+
+exports.token = process.env.DISCORD; //leave blank to disable Discord
+exports.logch = []; // Please do not use this without permission from a room owner
+
+exports.owner = 'YOUR_USERNAME_HERE';
+exports.admin = ["751433045529329825"]; // Array of Discord IDs of administrators
