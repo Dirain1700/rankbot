5a852ae5a56c189dfbc21d621d0a4520a9caa0f4
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-01-19 13:53:15 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 /* eslint-disable */
 const http = require("http");
 const querystring = require("querystring");
-const fs = require("fs")
-const pages = fs.readFileSync("./metas/index.html")
+const fs = require("fs");
+global.config = require("./config/config");
+const pages = fs.readFileSync("./config/index.html");
 const {Intents,Client,ClientApplication,Discord} = require('discord.js');
 const options = {
    intents: ["GUILDS", "GUILD_MESSAGES", "GUILD_VOICE_STATES", "GUILD_PRESENCES", "GUILD_MESSAGE_REACTIONS", "GUILD_MEMBERS", "GUILD_BANS","GUILD_MESSAGE_REACTIONS"],
