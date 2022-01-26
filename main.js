37ecf9544794aa622bdec544dfdbd8dd2b7974bc
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-01-26 15:48:06 +0000

    fix error

--- main.js
+++ main.js
@@ -333,7 +333,7 @@ module.exports = (client) => {
     const db = JSON.parse(fs.readFileSync("./rank.json"));
     const obj = Object.entries(db);
     obj.sort((a, b) => b[1].points - a[1].points);
-    const edit = JSON.stringify(Object.fromEntries(tmp), null, 4);
+    const edit = JSON.stringify(Object.fromEntries(obj), null, 4);
     fs.writeFileSync("./rank.json", edit);
     return;
   }
