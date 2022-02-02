360f1a58c3100a9f1f7e0b0c18d6da64a7976a38
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-02 11:20:50 +0900

    disable eslint at L12
    //eslint-disable-next-line no-unused-vars
    const cmd = require("./config/command.js");

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ module.exports = (client) => {
     console.log(`Logged in as ${client.user.tag}`);
     client.user.setActivity("ウマ娘 プリティーダービー Season2", { type: "WATCHING" }, { status: "busy" });
     ranksort();
+    //eslint-disable-next-line no-unused-vars
     const cmd = require("./config/command.js");
   });
   
