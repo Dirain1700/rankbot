4e61e044c117b9c5d8ba0beb3855257f97458592
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-07 16:23:27 +0900

    fix error

--- discord/ready.js
+++ discord/ready.js
@@ -2,5 +2,5 @@ module.exports = async client => {
   console.log(`Logged in as ${client.user.tag}`);
   client.user.setActivity("ウマ娘 プリティーダービー Season2", { type: "WATCHING" }, { status: "busy" });
   //eslint-disable-next-line no-unused-vars
-  const cmd = require("./config/command.js");
+  const cmd = require("./../config/command.js");
 };
