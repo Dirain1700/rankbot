0ed7ca381ae3939fdc24a8d618cfa324366fe426
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-07 13:15:07 +0900

    fix error

--- discord/index.js
+++ discord/index.js
@@ -1,6 +1,6 @@
 module.exports = async client => {
   client.on("interactionCreate", interaction => {
-    if (interaction.commandName = "ping") {
+    if (interaction.commandName === "ping") {
       const run = require("./interaction/ping")
       run(interaction);
     }
