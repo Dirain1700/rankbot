6d51d936f4aba1bd3a6c85a4a303c057ba6b07f8
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-07 16:11:18 +0900

    Create runjs.js

---
+++ showdown/chat/runjs.js
@@ -0,0 +1,23 @@
+module.exports = ps => {
+  const path = require("path");
+  const pool = require("workerpool").pool(path.join(__dirname, "./../../vm2/worker.js"), {
+    workerType: "process",
+  });
+  const content = message.content.replace(">runjs ", "");
+  const codeBlockRegex = /^`{2}(?<code>[\s\S]+)`{2}$/mu;
+  const toMessageOptions = result => {
+    if (result.length <= 2000)
+      return "``" + result + "``";
+    else return "too long result.";
+  };
+  
+  if (!codeBlockRegex.test(content))
+    return message.reply("Please send code!").catch(console.error);
+  const code = content.match(codeBlockRegex)?.groups ?? {};
+  
+  pool
+    .exec("run", [code.code])
+    .timeout(5000)
+    .then(result => message.reply(toMessageOptions(result)))
+    .catch(error => message.reply("``" + error + "``"));
+}
