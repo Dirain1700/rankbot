e53f0a2f804aa6a95bbcd75d362816750401a7df
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-07 21:35:34 +0900

    Update clearboard.js

--- discord/interaction/points/clearboard.js
+++ discord/interaction/points/clearboard.js
@@ -3,6 +3,6 @@ module.exports = interaction => {
   if (!interaction.user.id !== config.admin) {
     return interaction.reply({ content: "/clearleaderboard - Access denied.", ephemeral: true});
   }
-  fs.writeFileSync("../../config/rank.json", "{}");
+  fs.writeFileSync("./../../config/rank.json", "{}");
   interaction.reply("Reset successed!");
 };
