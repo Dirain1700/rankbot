7ebc8bb1ebc13e9da993db600af45e45586f0537
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-07 10:37:41 +0900

    Create rpt.js

---
+++ discord/interaction/rpt.js
@@ -0,0 +1,20 @@
+module.exports = interaction => {
+  if (!interaction.memberPermissions.has("ADMINISTRATOR")) {
+    return interaction.reply({ content: "/rpt - Access Denied.", ephemeral: true });
+  }
+  const score = interaction.options.getInteger("points");
+  const targetUser = interaction.options.getUser("user");
+  const db = JSON.parse(fs.readFileSync(".../rank.json"));
+  if (targetUser.id in db) { //ユーザーIDのデータがあるか判定
+    // ポイント加算
+    db[targetUser.id].points -= score;
+  } else {
+    return interaction.reply({ content: `Error: ${targetUser.tag} has no ranks.`, ephemeral: true });
+  }
+  // 書き換え
+  fs.writeFileSync(".../rank.json", JSON.stringify(db, null, 2));
+  //送信
+  interaction.reply(`Added ${score}points to ${targetUser.tag} and having ${db[targetUser.id].points}points now.`);
+  const { ranksort } = require("../ranksort");
+  ranksort();
+};
