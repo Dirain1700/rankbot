e809585227c2245522792e283a8aa1f148eaddc5
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-07 10:27:56 +0900

    Update ranksort.js

--- discord/ranksort.js
+++ discord/ranksort.js
@@ -4,4 +4,4 @@ module.exports = (() => {
   obj.sort((a, b) => b[1].points - a[1].points);
   const edit = JSON.stringify(Object.fromEntries(obj), null, 4);
   fs.writeFileSync("../rank.json", edit);
-};
+});
