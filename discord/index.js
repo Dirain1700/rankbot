4a585676470415c0e2c191a203e430b1cc8eacce
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-07 13:13:48 +0900

    Update index.js

--- discord/index.js
+++ discord/index.js
@@ -1,7 +1,8 @@
 module.exports = async client => {
   client.on("interactionCreate", interaction => {
     if (interaction.commandName = "ping") {
-      require("./interaction/ping")(client);
+      const run = require("./interaction/ping")
+      run(interaction);
     }
   });
 };
