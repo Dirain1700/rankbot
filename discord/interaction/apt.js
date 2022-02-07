ad6d024d6f5de12662354ac75093119e4a073f93
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-07 10:20:48 +0900

    Update apt.js

--- discord/interaction/apt.js
+++ discord/interaction/apt.js
@@ -3,20 +3,20 @@ module.exports = interaction => {
     interaction.reply({ content: "/apt - Access Denied.", ephemeral: true });
     return;
   }
-  const how = interaction.options.getInteger("points");
-  const target = interaction.options.getMember("user");
+  const score = interaction.options.getInteger("points");
+  const targetUser = interaction.options.getUser("user");
   const db = JSON.parse(fs.readFileSync("./rank.json"));
-  if (target.user.id in db) { //ユーザーIDのデータがあるか判定
+  if (targetUser.id in db) { //ユーザーIDのデータがあるか判定
     // ポイント加算
-    db[target.user.id].points += how;
+    db[targetUser.id].points += score;
   } else {
     // ポイント設定
-    db[target.user.id] = { points: how };
+    db[targetUser.id] = { points: score };
   }
   // 書き換え
   fs.writeFileSync("./rank.json", JSON.stringify(db, null, 2));
   //送信
-  interaction.reply(`Added ${how}points to ${target.user.tag} and having ${db[target.user.id].points}points now.`);
-  //eslint-disabble-next-line
+  interaction.reply(`Added ${score}points to ${targetUser.tag} and having ${db[targetUser.id].points}points now.`);
+  const ranksort = require("../ranksort");
   ranksort();
 }
