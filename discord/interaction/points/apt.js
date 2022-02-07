d8b356e0e867560fb9b5cd4cdfb9af19314c3bd5
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-07 23:23:50 +0900

    Update apt.js

--- discord/interaction/points/apt.js
+++ discord/interaction/points/apt.js
@@ -17,6 +17,6 @@ module.exports = interaction => {
   fs.writeFileSync("./../../config/rank.json", JSON.stringify(db, null, 2));
   //送信
   interaction.reply(`Added ${score}points to ${targetUser.tag} and having ${db[targetUser.id].points}points now.`);
-  const { ranksort } = require("./../../ranksort");
-  ranksort();
+  const sort = require("./../../ranksort");
+  sort();
 };
