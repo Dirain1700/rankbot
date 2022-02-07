d58ae9453fd5ad18c22ac6bcdb83f40a6fb45b7e
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-07 16:56:42 +0900

    Update index.js

--- showdown/index.js
+++ showdown/index.js
@@ -7,11 +7,29 @@ module.exports = (client, ps) => {
   
   ps.on("message", async message => {
     if (message.type !== "pm" || message.author.name === ps.status.username) return;
-    if (message.content.startsWith("/invite") {
+    if (message.content.startsWith("/invite")) {
       const run = require("./pm/invite");
       run(message);
     }else{
       const run = require("./commands");
       run(message);
+    }
+  });
+  
+  ps.on("message", async message => {
+    if (message.isIntro || message.type !== "chat" || message.author.name === ps.status.username) return;
+    if (message.target.roomid = "japanese") logmsg(message);
+    if (message.content.startsWith("/log") && (message.target.roomid).includes("japanese")) {
+      const run = require("./chat/sendlog");
+      run(client, message);
+    }
+    if (message.cotent.startsWith(">runjs")) {
+      const run = require("./chat/runjs");
+      run(message);
+    }
+    else {
+      const run = require("./commands")
+      run(message);
+    }
   });
 };
