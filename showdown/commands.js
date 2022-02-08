42c8ff538e138430624b7bcaea96bc8328fb8b69
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-08 10:27:44 +0900

    Update commands.js

--- showdown/commands.js
+++ showdown/commands.js
@@ -2,7 +2,7 @@ module.exports = message => {
   if (message.content.startsWith(".echo")) {
    return message.reply(message.content.replace(".echo ", ""));
   }else if (message.content === ".resetlog" && message.target.roomid === "japanese") {
-    message.reply(`japanese|ログの削除が完了しました。`);
+    message.reply("ログの削除が完了しました。");
     setTimeout(() => {
       fs.writeFileSync("./../config/log/chatlog.json", "[]");
     }, 500);
