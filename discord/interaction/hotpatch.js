b2625b00e2b93a34861ad758cb9d567488f20bc4
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-10 03:50:43 +0000

    Update hotpatch.js

--- discord/interaction/hotpatch.js
+++ discord/interaction/hotpatch.js
@@ -1,8 +1,20 @@
-module.exports = interaction => {
+module.exports = async interaction => {
   if (interaction.user.id !== config.admin[0]) {
     return interaction.reply({ content: "Access denied.", ephemeral: true });
   }
   const fileName = interaction.options.getString("module");
+  if (fileName === "git") {
+    const { execSync } = require("child_process");
+    try {
+      const result = execSync("git pull", { stdio: 'inherit' }).toString("utf-8");
+      execSync("git pull", { stdio: 'inherit' });
+      await interaction.reply(result);
+      return;
+    }catch(e){
+      interaction.reply(e);
+      return;
+    }
+  }
   const run = require("./../hotpatch");
   run(fileName, interaction);
 };
