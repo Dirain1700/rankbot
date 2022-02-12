753cfc642bbca355c1e10d5daec046863b2cc690
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-12 11:47:54 +0900

    Create formats.js

---
+++ showdown/tour/formats.js
@@ -0,0 +1,927 @@
+exports.formatList = [
+  {
+    "name": "[Gen 8] Random Battle",
+    "ruleset": [
+      "PotD",
+      "Obtainable",
+      "Species Clause",
+      "HP Percentage Mod",
+      "Cancel Mod",
+      "Sleep Clause Mod"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] OU",
+    "ruleset": [
+      "Standard",
+      "Dynamax Clause"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] Ubers",
+    "ruleset": [
+      "Standard",
+      "Dynamax Clause"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] UU",
+    "ruleset": [
+      "[Gen 8] OU"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] LC",
+    "ruleset": [
+      "Little Cup",
+      "Standard",
+      "Dynamax Clause"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] Monotype",
+    "ruleset": [
+      "Same Type Clause",
+      "Standard",
+      "Dynamax Clause"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] 1v1",
+    "ruleset": [
+      "Picked Team Size = 1",
+      "Max Team Size = 3",
+      "Obtainable",
+      "Species Clause",
+      "Nickname Clause",
+      "OHKO Clause",
+      "Evasion Moves Clause",
+      "Accuracy Moves Clause",
+      "Team Preview",
+      "HP Percentage Mod",
+      "Cancel Mod",
+      "Dynamax Clause",
+      "Endless Battle Clause"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] Anything Goes",
+    "ruleset": [
+      "Obtainable",
+      "Team Preview",
+      "HP Percentage Mod",
+      "Cancel Mod",
+      "Endless Battle Clause"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] ZU",
+    "ruleset": [
+      "[Gen 8] PU"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] LC UU",
+    "ruleset": [
+      "[Gen 8] LC"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] CAP",
+    "ruleset": [
+      "[Gen 8] OU",
+      "+CAP"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] CAP LC",
+    "ruleset": [
+      "[Gen 8] LC",
+      "+CAP"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] Battle Stadium Singles",
+    "ruleset": [
+      "Flat Rules",
+      "!! Adjust Level = 50",
+      "Min Source Gen = 8",
+      "Limit Two Restricted"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] Random Doubles Battle",
+    "ruleset": [
+      "PotD",
+      "Obtainable",
+      "Species Clause",
+      "HP Percentage Mod",
+      "Cancel Mod"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] Doubles OU",
+    "ruleset": [
+      "Standard Doubles",
+      "Dynamax Clause",
+      "Swagger Clause"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] Doubles Ubers",
+    "ruleset": [
+      "Standard Doubles",
+      "!Gravity Sleep Clause"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] Doubles UU",
+    "ruleset": [
+      "[Gen 8] Doubles OU"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] Doubles LC",
+    "ruleset": [
+      "Standard Doubles",
+      "Little Cup",
+      "Dynamax Clause",
+      "Swagger Clause",
+      "Sleep Clause Mod"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] VGC 2022",
+    "ruleset": [
+      "Flat Rules",
+      "!! Adjust Level = 50",
+      "Min Source Gen = 8",
+      "VGC Timer",
+      "Limit Two Restricted"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] VGC 2021",
+    "ruleset": [
+      "Flat Rules",
+      "!! Adjust Level = 50",
+      "Min Source Gen = 8",
+      "VGC Timer"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] VGC 2020",
+    "ruleset": [
+      "Flat Rules",
+      "!! Adjust Level = 50",
+      "Min Source Gen = 8",
+      "VGC Timer"
+    ],
+    "mod": "gen8dlc1"
+  },
+  {
+    "name": "[Gen 8] 2v2 Doubles",
+    "ruleset": [
+      "Picked Team Size = 2",
+      "Max Team Size = 4",
+      "Standard Doubles",
+      "Accuracy Moves Clause",
+      "Dynamax Clause",
+      "Sleep Clause Mod"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] Metronome Battle",
+    "ruleset": [
+      "Max Team Size = 2",
+      "HP Percentage Mod",
+      "Cancel Mod"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] National Dex",
+    "ruleset": [
+      "Standard NatDex",
+      "OHKO Clause",
+      "Evasion Moves Clause",
+      "Species Clause",
+      "Dynamax Clause",
+      "Sleep Clause Mod"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] National Dex AG",
+    "ruleset": [
+      "Standard NatDex"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] Random Dex",
+    "ruleset": [
+      "Standard NatDex",
+      "OHKO Clause",
+      "Evasion Moves Clause",
+      "Species Clause",
+      "Dynamax Clause",
+      "Sleep Clause Mod"
+    ],
+    "mod": "randomdex"
+  },
+  {
+    "name": "[Gen 8 BDSP] Random Battle",
+    "ruleset": [
+      "[Gen 8] Random Battle",
+      "!PotD"
+    ],
+    "mod": "gen8bdsp"
+  },
+  {
+    "name": "[Gen 8 BDSP] OU",
+    "ruleset": [
+      "Standard"
+    ],
+    "mod": "gen8bdsp"
+  },
+  {
+    "name": "[Gen 8 BDSP] Ubers",
+    "ruleset": [
+      "Standard"
+    ],
+    "mod": "gen8bdsp"
+  },
+  {
+    "name": "[Gen 8 BDSP] UU",
+    "ruleset": [
+      "[Gen 8 BDSP] OU"
+    ],
+    "mod": "gen8bdsp"
+  },
+  {
+    "name": "[Gen 8 BDSP] RU",
+    "ruleset": [
+      "[Gen 8 BDSP] UU"
+    ],
+    "mod": "gen8bdsp"
+  },
+  {
+    "name": "[Gen 8 BDSP] NU",
+    "ruleset": [
+      "[Gen 8 BDSP] RU"
+    ],
+    "mod": "gen8bdsp"
+  },
+  {
+    "name": "[Gen 8 BDSP] LC",
+    "ruleset": [
+      "Little Cup",
+      "Standard"
+    ],
+    "mod": "gen8bdsp"
+  },
+  {
+    "name": "[Gen 8 BDSP] Monotype",
+    "ruleset": [
+      "Standard",
+      "Same Type Clause"
+    ],
+    "mod": "gen8bdsp"
+  },
+  {
+    "name": "[Gen 8 BDSP] 3v3 Singles",
+    "ruleset": [
+      "Flat Rules",
+      "Min Source Gen = 8"
+    ],
+    "mod": "gen8bdsp"
+  },
+  {
+    "name": "[Gen 8 BDSP] Doubles OU",
+    "ruleset": [
+      "Standard Doubles"
+    ],
+    "mod": "gen8bdsp"
+  },
+  {
+    "name": "[Gen 8 BDSP] Battle Festival Doubles",
+    "ruleset": [
+      "Flat Rules",
+      "Min Source Gen = 8"
+    ],
+    "mod": "gen8bdsp"
+  },
+  {
+    "name": "[Gen 8 BDSP] Pure Hackmons",
+    "ruleset": [
+      "-Nonexistent",
+      "Team Preview",
+      "HP Percentage Mod",
+      "Cancel Mod",
+      "Endless Battle Clause"
+    ],
+    "mod": "gen8bdsp"
+  },
+  {
+    "name": "[Gen 8] The Loser's Game",
+    "ruleset": [
+      "Standard",
+      "!OHKO Clause",
+      "Picked Team Size = 6",
+      "Adjust Level = 100"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] Monotype Random Battle",
+    "ruleset": [
+      "Obtainable",
+      "Same Type Clause",
+      "HP Percentage Mod",
+      "Cancel Mod",
+      "Sleep Clause Mod"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] Random Battle (No Dmax)",
+    "ruleset": [
+      "[Gen 8] Random Battle",
+      "Dynamax Clause"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] Random Battle Mayhem",
+    "ruleset": [
+      "[Gen 8] Random Battle",
+      "Team Preview",
+      "Dynamax Clause",
+      "Camomons Mod",
+      "Inverse Mod",
+      "Scalemons Mod"
+    ],
+    "mod": "sharedpower"
+  },
+  {
+    "name": "[Gen 8] BSS Factory",
+    "ruleset": [
+      "Flat Rules"
+    ]
+  },
+  {
+    "name": "[Gen 8] Super Staff Bros 4",
+    "ruleset": [
+      "Dynamax Clause",
+      "HP Percentage Mod",
+      "Cancel Mod",
+      "Sleep Clause Mod"
+    ],
+    "mod": "ssb"
+  },
+  {
+    "name": "[Gen 8] Challenge Cup 1v1",
+    "ruleset": [
+      "[Gen 8] Challenge Cup",
+      "Team Preview",
+      "Dynamax Clause",
+      "Picked Team Size = 1"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 8] Challenge Cup 2v2",
+    "ruleset": [
+      "[Gen 8] Challenge Cup 1v1",
+      "!! Picked Team Size = 2"
+    ],
+    "mod": "gen8"
+  },
+  {
+    "name": "[Gen 7] Random Battle",
+    "ruleset": [
+      "Obtainable",
+      "Sleep Clause Mod",
+      "HP Percentage Mod",
+      "Cancel Mod"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 7] Random Doubles Battle",
+    "ruleset": [
+      "Obtainable",
+      "HP Percentage Mod",
+      "Cancel Mod"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 7] Battle Factory",
+    "ruleset": [
+      "Obtainable",
+      "Sleep Clause Mod",
+      "Team Preview",
+      "HP Percentage Mod",
+      "Cancel Mod",
+      "Mega Rayquaza Clause"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 7] BSS Factory",
+    "ruleset": [
+      "Flat Rules"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 7] Hackmons Cup",
+    "ruleset": [
+      "HP Percentage Mod",
+      "Cancel Mod"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 7 Let's Go] Random Battle",
+    "ruleset": [
+      "Obtainable",
+      "Allow AVs",
+      "HP Percentage Mod",
+      "Cancel Mod",
+      "Sleep Clause Mod"
+    ],
+    "mod": "gen7letsgo"
+  },
+  {
+    "name": "[Gen 6] Random Battle",
+    "ruleset": [
+      "Obtainable",
+      "Sleep Clause Mod",
+      "HP Percentage Mod",
+      "Cancel Mod"
+    ],
+    "mod": "gen6"
+  },
+  {
+    "name": "[Gen 6] Battle Factory",
+    "ruleset": [
+      "Obtainable",
+      "Sleep Clause Mod",
+      "Team Preview",
+      "HP Percentage Mod",
+      "Cancel Mod",
+      "Mega Rayquaza Clause"
+    ],
+    "mod": "gen6"
+  },
+  {
+    "name": "[Gen 5] Random Battle",
+    "ruleset": [
+      "Obtainable",
+      "Sleep Clause Mod",
+      "HP Percentage Mod",
+      "Cancel Mod"
+    ],
+    "mod": "gen5"
+  },
+  {
+    "name": "[Gen 4] Random Battle",
+    "ruleset": [
+      "Obtainable",
+      "Sleep Clause Mod",
+      "HP Percentage Mod",
+      "Cancel Mod"
+    ],
+    "mod": "gen4"
+  },
+  {
+    "name": "[Gen 3] Random Battle",
+    "ruleset": [
+      "Standard"
+    ],
+    "mod": "gen3"
+  },
+  {
+    "name": "[Gen 2] Random Battle",
+    "ruleset": [
+      "Standard"
+    ],
+    "mod": "gen2"
+  },
+  {
+    "name": "[Gen 1] Random Battle",
+    "ruleset": [
+      "Standard"
+    ],
+    "mod": "gen1"
+  },
+  {
+    "name": "[Gen 7] OU",
+    "ruleset": [
+      "Standard"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 6] OU",
+    "ruleset": [
+      "Standard",
+      "Swagger Clause"
+    ],
+    "mod": "gen6"
+  },
+  {
+    "name": "[Gen 5] OU",
+    "ruleset": [
+      "Standard",
+      "Evasion Abilities Clause",
+      "Sleep Moves Clause",
+      "Swagger Clause"
+    ],
+    "mod": "gen5"
+  },
+  {
+    "name": "[Gen 4] OU",
+    "ruleset": [
+      "Standard"
+    ],
+    "mod": "gen4"
+  },
+  {
+    "name": "[Gen 3] OU",
+    "ruleset": [
+      "Standard",
+      "One Boost Passer Clause"
+    ],
+    "mod": "gen3"
+  },
+  {
+    "name": "[Gen 2] OU",
+    "ruleset": [
+      "Standard"
+    ],
+    "mod": "gen2"
+  },
+  {
+    "name": "[Gen 1] OU",
+    "ruleset": [
+      "Standard"
+    ],
+    "mod": "gen1"
+  },
+  {
+    "name": "[Gen 7] Ubers",
+    "ruleset": [
+      "Standard",
+      "Mega Rayquaza Clause"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 7] UU",
+    "ruleset": [
+      "[Gen 7] OU"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 7] LC",
+    "ruleset": [
+      "Little Cup",
+      "Standard",
+      "Swagger Clause"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 7] Monotype",
+    "ruleset": [
+      "Same Type Clause",
+      "Standard",
+      "Swagger Clause"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 7] 1v1",
+    "ruleset": [
+      "Picked Team Size = 1",
+      "Max Team Size = 3",
+      "Obtainable",
+      "Species Clause",
+      "Nickname Clause",
+      "OHKO Clause",
+      "Swagger Clause",
+      "Evasion Moves Clause",
+      "Accuracy Moves Clause",
+      "Team Preview",
+      "HP Percentage Mod",
+      "Cancel Mod",
+      "Endless Battle Clause"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 7] Anything Goes",
+    "ruleset": [
+      "Obtainable",
+      "Team Preview",
+      "HP Percentage Mod",
+      "Cancel Mod",
+      "Endless Battle Clause"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 7] Battle Spot Singles",
+    "ruleset": [
+      "Flat Rules",
+      "Min Source Gen = 6"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 7 Let's Go] OU",
+    "ruleset": [
+      "Adjust Level = 50",
+      "Obtainable",
+      "Species Clause",
+      "Nickname Clause",
+      "OHKO Clause",
+      "Evasion Moves Clause",
+      "Team Preview",
+      "HP Percentage Mod",
+      "Cancel Mod",
+      "Sleep Clause Mod"
+    ],
+    "mod": "gen7letsgo"
+  },
+  {
+    "name": "[Gen 7] Doubles OU",
+    "ruleset": [
+      "Standard Doubles",
+      "Swagger Clause"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 7] VGC 2019",
+    "ruleset": [
+      "Flat Rules",
+      "!! Adjust Level = 50",
+      "Min Source Gen = 7",
+      "VGC Timer",
+      "Limit Two Restricted"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 7] VGC 2018",
+    "ruleset": [
+      "Flat Rules",
+      "!! Adjust Level = 50",
+      "Min Source Gen = 7"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 7] VGC 2017",
+    "ruleset": [
+      "Flat Rules",
+      "Old Alola Pokedex",
+      "!! Adjust Level = 50",
+      "Min Source Gen = 7"
+    ],
+    "mod": "gen7sm"
+  },
+  {
+    "name": "[Gen 7] Battle Spot Doubles",
+    "ruleset": [
+      "Flat Rules",
+      "Min Source Gen = 6"
+    ],
+    "mod": "gen7"
+  },
+  {
+    "name": "[Gen 6] 1v1",
+    "ruleset": [
+      "Max Team Size = 3",
+      "Picked Team Size = 1",
+      "Obtainable",
+      "Nickname Clause",
+      "Moody Clause",
+      "OHKO Clause",
+      "Evasion Moves Clause",
+      "Accuracy Moves Clause",
+      "Swagger Clause",
+      "Endless Battle Clause",
+      "HP Percentage Mod",
+      "Cancel Mod",
+      "Team Preview"
+    ],
+    "mod": "gen6"
+  },
+  {
+    "name": "[Gen 6] Battle Spot Singles",
+    "ruleset": [
+      "Flat Rules",
+      "Min Source Gen = 6"
+    ],
+    "mod": "gen6"
+  },
+  {
+    "name": "[Gen 6] Doubles OU",
+    "ruleset": [
+      "Standard Doubles",
+      "Swagger Clause"
+    ],
+    "mod": "gen6"
+  },
+  {
+    "name": "[Gen 6] VGC 2016",
+    "ruleset": [
+      "Flat Rules",
+      "Min Source Gen = 6",
+      "Limit Two Restricted"
+    ],
+    "mod": "gen6"
+  },
+  {
+    "name": "[Gen 6] VGC 2015",
+    "ruleset": [
+      "Flat Rules",
+      "Min Source Gen = 6"
+    ],
+    "mod": "gen6"
+  },
+  {
+    "name": "[Gen 6] VGC 2014",
+    "ruleset": [
+      "Flat Rules",
+      "Kalos Pokedex",
+      "Min Source Gen = 6"
+    ],
+    "mod": "gen6xy"
+  },
+  {
+    "name": "[Gen 6] Battle Spot Doubles",
+    "ruleset": [
+      "Flat Rules",
+      "Min Source Gen = 6"
+    ],
+    "mod": "gen6"
+  },
+  {
+    "name": "[Gen 6] Battle Spot Triples",
+    "ruleset": [
+      "Flat Rules",
+      "Min Source Gen = 6"
+    ],
+    "mod": "gen6"
+  },
+  {
+    "name": "[Gen 5] 1v1",
+    "ruleset": [
+      "Picked Team Size = 1",
+      "Max Team Size = 3",
+      "Standard",
+      "Baton Pass Clause",
+      "Swagger Clause",
+      "Accuracy Moves Clause"
+    ],
+    "mod": "gen5"
+  },
+  {
+    "name": "[Gen 5] GBU Singles",
+    "ruleset": [
+      "Flat Rules"
+    ],
+    "mod": "gen5"
+  },
+  {
+    "name": "[Gen 5] Doubles OU",
+    "ruleset": [
+      "Standard",
+      "Evasion Abilities Clause",
+      "Swagger Clause",
+      "Sleep Clause Mod"
+    ],
+    "mod": "gen5"
+  },
+  {
+    "name": "[Gen 5] VGC 2013",
+    "ruleset": [
+      "Flat Rules"
+    ],
+    "mod": "gen5"
+  },
+  {
+    "name": "[Gen 5] VGC 2012",
+    "ruleset": [
+      "Flat Rules"
+    ],
+    "mod": "gen5bw1"
+  },
+  {
+    "name": "[Gen 5] VGC 2011",
+    "ruleset": [
+      "Flat Rules",
+      "Old Unova Pokedex"
+    ],
+    "mod": "gen5bw1"
+  },
+  {
+    "name": "[Gen 4] 1v1",
+    "ruleset": [
+      "Picked Team Size = 1",
+      "Max Team Size = 3",
+      "[Gen 4] OU",
+      "Accuracy Moves Clause",
+      "Sleep Moves Clause",
+      "Team Preview"
+    ],
+    "mod": "gen4"
+  },
+  {
+    "name": "[Gen 4] Anything Goes",
+    "ruleset": [
+      "Obtainable",
+      "Arceus EV Limit",
+      "Endless Battle Clause",
+      "HP Percentage Mod",
+      "Cancel Mod"
+    ],
+    "mod": "gen4"
+  },
+  {
+    "name": "[Gen 4] Doubles OU",
+    "ruleset": [
+      "[Gen 4] OU"
+    ],
+    "mod": "gen4"
+  },
+  {
+    "name": "[Gen 4] VGC 2010",
+    "ruleset": [
+      "Flat Rules",
+      "Max Team Size = 4",
+      "Limit Two Restricted"
+    ],
+    "mod": "gen4"
+  },
+  {
+    "name": "[Gen 4] VGC 2009",
+    "ruleset": [
+      "Flat Rules",
+      "! Adjust Level Down",
+      "Max Level = 50",
+      "Max Team Size = 4"
+    ],
+    "mod": "gen4pt"
+  },
+  {
+    "name": "[Gen 3] Doubles OU",
+    "ruleset": [
+      "Standard",
+      "!Sleep Clause Mod",
+      "!Switch Priority Clause Mod"
+    ],
+    "mod": "gen3"
+  },
+  {
+    "name": "[Gen 3] 1v1",
+    "ruleset": [
+      "Picked Team Size = 1",
+      "Max Team Size = 3",
+      "[Gen 3] OU",
+      "Accuracy Moves Clause",
+      "Sleep Moves Clause",
+      "Team Preview"
+    ],
+    "mod": "gen3"
+  }
+]
