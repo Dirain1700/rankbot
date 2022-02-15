76e5c0a20b7eba06e3c7211e3871cdb55bcb9d3b
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-02-15 17:36:47 +0900

    Update tours.js

--- showdown/tour/tours.js
+++ showdown/tour/tours.js
@@ -2,7 +2,7 @@ module.exports = (message, option) => {
   if (option === "bsss9"); {
     const commands = [
       "/tour new bss",
-      "/tour rules -Restricted Legendary, vgctimer"
+      "/tour rules -Restricted Legendary, vgctimer",
       "/tour name [Gen 8] Battle Stadium Singles S9"
     ];
     message.reply(commands.join("\n"));
