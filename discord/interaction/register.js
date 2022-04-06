80fabb13ae2ffcaca2bd5ab718a185eca41b1589
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-04-06 09:51:25 +0900

    Update register.js

--- discord/interaction/register.js
+++ discord/interaction/register.js
@@ -4,26 +4,28 @@ module.exports = async (client, interaction, PSClient) => {
     if (!userid) return void interaction.reply({ content: `${interaction.options.getString("userid", true)} is an invalid userid.`, ephemeral: true });
     PSClient.pending.set(userid, interaction.user.id);
     interaction.reply({ content: `Waiting for you to send the ?register command in Pokémon Showdown. PM to Dirain1700 with the content "?register ${interaction.user.id}" in 10 minutes!`, fetchReply: true, ephmeral: true });
-    return void new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         // eslint-disable-next-line no-unused-vars
         PSClient.on("message", function (message) {
-            if (this.isIntro || this.author.id !== userid || !this.content.startsWith("?register ")) return;
+            if (this.isIntro || this.type !== "pm" || this.author.id !== userid || !this.content.startsWith("?register ")) return;
             const inputUserid = this.content.substring(10);
             if (!UserRegex.test(inputUserid) || !client.users.cache.get(inputUserid)) return void this.reply("Error: The id of Discord that you input was invalid.");
             if (userid !== this.parent.pending.get(this.author.userid)) return void this.reply("Error: The id of Discord that you input was not found in registering list. Type \"/register [userid]\" in Discord.");
             if (!this.author.autoconfirmed) return void this.reply("Your account is not autoconfirmed annount. Try after you got autoconfirmed!\n!faq ac");
             interaction.member.roles.add(interaction.roles.cache.get(config.acRole));
-            interaction.fetchReply()
-                .then(m => m.edit(`Sucessfully registerd your account as "${this.username}" and added <&${config.acRole}> role.`))
-                .catch(console.error);
+            this.reply("Registration sucessed!");
             resolve();
-            return;
         });
         setTimeout(() => {
-            interaction.fetchReply()
-                .then(m => m.edit("Failed to Registration: Timeout!"))
-                .catch(console.error);
             reject();
         }, 10 * 60 * 1000);
-    });
+    }).then(() => {
+        interaction.fetchReply()
+                .then(m => m.edit(`Sucessfully registerd your account as "${this.username}" and added <&${config.acRole}> role.`))
+                .catch(console.error);
+    }).catch(() => {
+        interaction.fetchReply()
+            .then(m => m.edit("Failed to Registration: Timeout!"))
+            .catch(console.error)
+    }).finally(() => PSClient.pending.delete(userid));
 };
