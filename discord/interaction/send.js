95d5e0cbd2df3a5a9e5f00addf02fba5062913d5
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-04-10 14:13:33 +0000

    bug fix

--- discord/interaction/send.js
+++ discord/interaction/send.js
@@ -5,16 +5,20 @@ module.exports = async (interaction) => {
     const type = interaction.options.getString("type", true);
     const channel = interaction.options.getChannel("channel", true);
 
+    interaction.reply(`Please send ${type} in 2 minutes!`);
+
     const awaitMessageFilter = (m) => m.author === interaction.user;
     const content = await interaction.channel
         .awaitMessages({
             filter: awaitMessageFilter,
-            max: 3,
-            idle: 5 * 60 * 1000,
+            max: 5,
+            idle: 3 * 60 * 1000,
             errors: ["idle"],
         })
-        .then((c) => c.map((m) => m.content));
+        .then((c) => c.map((m) => m.content))
+        .catch((c) => c.map((m) => m.content) || null);
 
+    if (!content) return void interaction.channel.send("Got empty message!");
     let message;
     switch (type) {
         case "string":
@@ -36,21 +40,22 @@ module.exports = async (interaction) => {
         }
         case "string with embed": {
             const embeds = [];
-            const objects = content.filter((e) => e.startsWith("embed: ")).map((e) => e.substring(7));
-            const string = content.filter((e) => e.startsWith("string: ")).map((e) => e.substring(8));
+            const objects = content.filter((e) => e.startsWith("{"));
+            const string = content.filter((e) => !e.startsWith("{"));
             try {
                 objects.forEach((e) => embeds.push(JSON.parse(e)));
             } catch (e) {
                 interaction.channel.send(e.toString());
             }
             try {
-                message = { content: string, embeds: embeds.map((e) => new MessageEmbed(e)) };
+                message = { content: string.join("\n"), embeds: embeds.map((e) => new MessageEmbed(e)) };
             } catch (e) {
                 interaction.channel.send(e.toString());
             }
             break;
         }
     }
+    interaction.channel.send(typeof message);
     if (!message) return void interaction.channel.send("Can't send empty message!");
     channel.send(message);
 };
