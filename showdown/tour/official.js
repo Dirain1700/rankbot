2fef10ff907550bbc9a3afd27cc7929671edf185
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-05-10 14:57:41 +0000

    tour: Refactoring and Update formats

--- showdown/tour/official.js
+++ showdown/tour/official.js
@@ -31,5 +31,5 @@ exports.announce = (client, room) => {
     client.sendRoom(room, `/announce 30分後から${format}のOfficial Tournamentを開催します!奮ってご参加ください!`);
     client.sendRoom(room, `/announce After 30 minutes , we will open an Official Tournament in ${format}! Please join with us!`);
     const randomized = ["Random", "Factory", "Hackmons", "Staff"];
-    if (!randomized.some((e) => format.includes(e))) client.sendRoom(room, `.steams ${format}`);
+    if (!randomized.some((e) => format.includes(e))) client.sendRoom(room, `!tier ${format}`);
 };
--- showdown/tour/tourmanager.js
+++ showdown/tour/tourmanager.js
@@ -12,7 +12,7 @@ exports.createTour = (message) => {
     run(message, option);
 };
 
-exports.setTourSettings = (client, room, msg, isIntro) => {
+exports.setTourSettings = async (client, room, msg, isIntro) => {
     const sleep = (t) => new Promise((r) => setTimeout(r, t));
     if (isIntro || !room.includes("japanese")) return;
     const event = msg.split("|")[0];
@@ -20,25 +20,21 @@ exports.setTourSettings = (client, room, msg, isIntro) => {
         if (config.tourSettings.length > 5) {
             let send;
             let i = 0;
-            send = setInterval(() => {
-                sleep(client.sendInterval)
-                    .then(() => () => {
-                        config.tourSettings.slice(i, i + 5)?.forEach((e) => this.send(`${room}|/tour ${e}`));
-                    })
-
-                    .finally(() => {
-                        if (i >= config.tourSettings.length) clearInterval(send);
-                        else i += 5;
-                    });
+            send = setInterval(async () => {
+                await sleep(client.sendInterval);
+                config.tourSettings.slice(i, i + 5)?.forEach((e) => this.send(`${room}|/tour ${e}`));
+                if (i >= config.tourSettings.length) clearInterval(send);
+                else i += 5;
             }, client.sendInterval + 50);
-        } else sleep(client.sendInterval).then(() => config.tourSettings?.forEach((e) => client.sendRoom(room, `/tour ${e}`)));
+        } else {
+            await sleep(client.sendInterval);
+            config.tourSettings?.forEach((e) => client.sendRoom(room, `/tour ${e}`));
+        }
         const format = msg.split("|")[1];
         const randomized = ["random", "factory", "hackmons", "staff"];
         if (!randomized.some((e) => format.includes(e))) {
-            sleep(client.sendInterval).then(() => {
-                client.sendRoom(room, "/tour scouting disallow");
-                client.sendRoom(room, `.steams ${format}`);
-            });
+            await sleep(client.sendInterval);
+            client.sendRoom(room, "/tour scouting disallow");
         }
     }
     /*
