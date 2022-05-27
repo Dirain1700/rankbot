bb08bb9efaf0e358e849d6b81fca2a53d5b243d2
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-05-27 12:53:00 +0900

    Update official.js

--- showdown/tour/official.js
+++ showdown/tour/official.js
@@ -11,10 +11,25 @@ exports.createTour = (client, room) => {
         return;
     }
     const format = tourSchedule[new Date().getDate()];
-    let command;
-    if (format?.includes("1v1")) command = `/tour new ${format}, rr`;
-    else command = `/tour new ${format}, elim`;
-    client.sendRoom(room, command);
+    if (!format) return void client.sendRoom(room, "Data not found.");
+    const command = [];
+    if (format.format.includes("1v1")) command.push(`/tour new ${format.format}, rr`);
+    else command.push(`/tour new ${format.format}, elim`);
+    
+    if (format.rules) format.rules.map((e) => "/tour rules " + e).forEach(command.push);
+    if (format.name) command.push(format.name);
+    
+    if (command.length > 5) {
+            let send;
+            let i = 0;
+            send = setInterval(async () => {
+                command.slice(i, i + 5)?.forEach((e) => client.send(`${room}|/tour ${e}`));
+                if (i >= command.length) clearInterval(send);
+                else i += 5;
+            }, client.sendInterval + 50);
+        } else {
+            command.forEach((e) => client.sendRoom(room, e));
+        }
 };
 
 exports.announce = (client, room) => {
@@ -28,8 +43,8 @@ exports.announce = (client, room) => {
         return;
     }
     const format = tourSchedule[new Date().getDate()];
-    client.sendRoom(room, `/announce 30分後から${format}のOfficial Tournamentを開催します!奮ってご参加ください!`);
-    client.sendRoom(room, `/announce After 30 minutes , we will open an Official Tournament in ${format}! Please join with us!`);
+    client.sendRoom(room, `/announce 30分後から${format.name}のOfficial Tournamentを開催します!奮ってご参加ください!`);
+    client.sendRoom(room, `/announce After 30 minutes , we will open an Official Tournament in ${format.name}! Please join with us!`);
     const randomized = ["Random", "Factory", "Hackmons", "Staff"];
-    if (!randomized.some((e) => format.includes(e))) client.sendRoom(room, `!tier ${format}`);
+    if (!randomized.some((e) => format.includes(e))) client.sendRoom(room, `!tier ${format.format}`);
 };
