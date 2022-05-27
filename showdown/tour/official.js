25382ee5de5e5fd50d646fcc6110f9d40ef79ea5
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-05-27 23:01:25 +0900

    Update official.js

--- showdown/tour/official.js
+++ showdown/tour/official.js
@@ -1,5 +1,15 @@
 "use strict";
 
+const forEachSend = (array, func, time) => {
+    let send;
+    let i = 0;
+    send = setInterval(async () => {
+        array.slice(i, i + 5)?.forEach((e) => func(`${room}|/tour ${e}`));
+        if (i >= array.length) clearInterval(send);
+        else i += 5;
+    }, time);
+});
+
 exports.createTour = (client, room) => {
     let month = new String(new Date().getMonth() + 1);
     month = month.length === 1 ? "0" + month : month;
@@ -10,26 +20,17 @@ exports.createTour = (client, room) => {
         if (e.code === "MODULE_NOT_FOUND") client.sendRoom(room, "!code " + e);
         return;
     }
-    const format = tourSchedule[new Date().getDate()];
+    const { format, name, rules } = tourSchedule[new Date().getDate()];
     if (!format) return void client.sendRoom(room, "Data not found.");
     const command = [];
-    if (format.format.includes("1v1")) command.push(`/tour new ${format.format}, rr`);
-    else command.push(`/tour new ${format.format}, elim`);
+    if (format.includes("1v1")) command.push(`/tour new ${format}, rr`);
+    else command.push(`/tour new ${format}, elim`);
 
-    if (format.rules) format.rules.map((e) => "/tour rules " + e).forEach(command.push);
-    if (format.name) command.push(format.name);
+    if (rules) rules.map((e) => "/tour rules " + e).forEach(command.push);
+    if (name) command.push(name);
 
-    if (command.length > 5) {
-        let send;
-        let i = 0;
-        send = setInterval(async () => {
-            command.slice(i, i + 5)?.forEach((e) => client.send(`${room}|/tour ${e}`));
-            if (i >= command.length) clearInterval(send);
-            else i += 5;
-        }, client.sendInterval + 50);
-    } else {
-        command.forEach((e) => client.sendRoom(room, e));
-    }
+    if (command.length > 5) forEachSend(command, client.send, client.messageInterval);
+    else command.forEach((e) => client.sendRoom(room, e));
 };
 
 exports.announce = (client, room) => {
@@ -42,9 +43,13 @@ exports.announce = (client, room) => {
         if (e.code === "MODULE_NOT_FOUND") client.sendRoom(room, "!code " + e);
         return;
     }
-    const { format, name } = tourSchedule[new Date().getDate()];
-    client.sendRoom(room, `/announce 30分後から${name}のOfficial Tournamentを開催します!奮ってご参加ください!`);
-    client.sendRoom(room, `/announce After 30 minutes , we will open an Official Tournament in ${name}! Please join with us!`);
+    const { format, name, rules } = tourSchedule[new Date().getDate()];
+    client.sendRoom(room, `/announce 30分後から${name ?? format}のOfficial Tournamentを開催します!奮ってご参加ください!`);
+    client.sendRoom(room, `/announce After 30 minutes , we will open an Official Tournament in ${name ?? format}! Please join with us!`);
     const randomized = ["Random", "Factory", "Hackmons", "Staff"];
     if (!randomized.some((e) => format.includes(e))) client.sendRoom(room, `!tier ${format}`);
+    if (rules) {
+        if (rules.length > 5) forEachSend(rules.map((e) => `${room}|!tier ${e}`), client.send, client.messageInterval);
+        else rules.forEach((e) => client.sendRoom(room, e));
+    }
 };
