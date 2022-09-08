e997d0fb94d9c7875c0298ac58e90d04854e8369
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-09-08 23:24:47 +0900

    fix typo

--- src/showdown/tour/tourmanager.ts
+++ src/showdown/tour/tourmanager.ts
@@ -5,10 +5,10 @@ import type { Room } from "@dirain/client";
 export default async (room: Room, format: string) => {
     if (!room.id.includes("japanese")) return;
     if (config.tourSettings.length > 5) {
-        PS.sendArray(config.tourSettings);
+        PS.sendArray(config.tourSettings.map((e) => `${room.id}|/tour ${e}`));
     } else {
         await Tools.sleep(PS.messageInterval);
-        config.tourSettings?.forEach((e) => PS.sendRoom(room.id, `/tour ${e}`));
+        config.tourSettings?.forEach?.((e) => PS.sendRoom(room.id, `/tour ${e}`));
     }
     const randomized = ["random", "factory", "hackmons", "staff"];
     if (randomized.some((e) => format.includes(e))) {
