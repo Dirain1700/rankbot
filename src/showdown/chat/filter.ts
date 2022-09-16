13bb2016ad9575c17d570c8477f309ceea458e8b
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-09-16 13:57:52 +0000

    chatFilter: Updates

--- src/showdown/chat/filter.ts
+++ src/showdown/chat/filter.ts
@@ -4,19 +4,44 @@ import type { Room, Message } from "@dirain/client";
 import type { GuildTextBasedChannel } from "discord.js";
 
 export default (message: Message<Room>): void => {
-    if (message.target.isStaff(message.author)) return;
+    if (message.target.isStaff(message.author) || /[\u{3000}-\u{301C}\u{3041}-\u{3093}\u{309B}-\u{309E}]/mu.test(message.content)) return;
     const stretchRegex: RegExp = /(.)\1{4,}/gimsu;
-    const wordStretchRegex: RegExp = /(\S+?)\1+\1+\1+/gimsu;
+    const wordStretchRegex: RegExp = /(\S+?)\1+\1+\1+\1+/gimsu;
+
     const stretchFilter: (str: string) => boolean = (str: string) => {
         const result = str.match(stretchRegex);
         if (Array.isArray(result)) return !str.toLowerCase().startsWith("w".repeat(5));
         else return false;
     };
+
     const wordStretchFilter: (str: string) => boolean = (str: string) => {
         const result = str.match(wordStretchRegex);
         if (Array.isArray(result)) return !str.toLowerCase().startsWith("w".repeat(5));
         else return false;
     };
+
+    const wordStretchWithBlankFilter: (str: string) => boolean = (str: string) => {
+        const arr = str.split(" ");
+        let latestWord = "";
+        const set = new Set();
+        const duplicatedWords: { [word: string]: number } = {};
+        let i = 1;
+        let count = 0;
+
+        for (const word of arr) {
+            set.add(word);
+            if (set.size !== arr.length) {
+                duplicatedWords[word] ? duplicatedWords[word]++ : (duplicatedWords[word] = 1);
+                arr.slice(i);
+            } else i++;
+            if (latestWord === word) count++;
+
+            latestWord = word;
+        }
+        if (count > 3) return true;
+        else return false;
+    };
+
     const noMeaningFilter: (str: string) => boolean = (str: string) => {
         if (str.length > 16) return false;
         str = str.toLowerCase();
@@ -53,12 +78,10 @@ export default (message: Message<Room>): void => {
     let content: string = "Caught by ";
 
     if (stretchFilter(message.content)) content += "stretchFilter: " + message.content;
-
-    if (wordStretchFilter(message.content)) content += "wordStretchFilter: " + message.content;
-
-    if (noMeaningFilter(message.content)) content += "noMeaningFilter: " + message.content;
-
-    if (content === "Caught by ") return;
+    else if (wordStretchFilter(message.content)) content += "wordStretchFilter: " + message.content;
+    else if (wordStretchWithBlankFilter(message.content)) content += "wordStretchFilter: " + message.content;
+    else if (noMeaningFilter(message.content)) content += "noMeaningFilter: " + message.content;
+    else return;
     message.reply(content);
     (discord.channels.cache.get(config.testCh) as GuildTextBasedChannel)?.send?.(content);
 };
