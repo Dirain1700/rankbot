2985c4fb3f559ff6b26e8a20734beb36a613e2ea
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-09-19 15:55:26 +0000

    enableModchat: fix error

--- src/showdown/chat/modchat.ts
+++ src/showdown/chat/modchat.ts
@@ -10,7 +10,7 @@ export const enableModchat = async (targetRoom: Room, targetUser: User) => {
     if (new Date().getHours() < 13) return;
     if (!targetRoom.isStaff(targetUser)) return;
     const users: User[] = (targetRoom.users as string[]).map((u) => PS.getUser(Tools.toId(u)) as User);
-    const Staffs = users.filter((u) => targetRoom.isStaff(u));
+    const Staffs = users.filter((u) => targetRoom.isStaff(u) && !targetRoom.isBot(u.id) && !u.isGlobalBot);
     const isActiveStaffOnline = !Staffs.length || Staffs.some((e) => !(e.status ?? "").startsWith("!(Idle) "));
     if (isActiveStaffOnline) return;
     PS.sendRoom("japanese", "/modchat +");
@@ -24,7 +24,7 @@ export const disableModchat = async (targetRoom: Room, targetUser: User) => {
     if (new Date().getHours() > 13) return;
     if (!targetRoom.isStaff(targetUser)) return;
     const users: User[] = (targetRoom.users as string[]).map((u) => PS.getUser(Tools.toId(u)) as User);
-    const Staffs = users.filter((u) => targetRoom.isStaff(u));
+    const Staffs = users.filter((u) => targetRoom.isStaff(u) && !targetRoom.isBot(u.id) && !u.isGlobalBot);
     const noActiveStaff = !Staffs.length || Staffs.every((e) => ["!(Idle) ", "!(Busy) "].includes((e.status ?? "").substring(8)));
     if (noActiveStaff) return;
     targetRoom.sendHTML({
