868f4518a6b836a0105b97cebe4201949310f8b4
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-09-20 16:22:26 +0000

    modchat: fix condition

--- src/showdown/chat/modchat.ts
+++ src/showdown/chat/modchat.ts
@@ -7,12 +7,15 @@ export const enableModchat = async (targetRoom: Room, targetUser: User) => {
     targetRoom = await PS.fetchRoom(targetRoom.id, false).catch(() => PS.getRoom(targetRoom.id)!);
 
     if (!targetRoom.modchat || !["off", "autoconfirmed"].includes(targetRoom.modchat)) return;
-    if (new Date().getHours() < 13) return;
+    const hour = new Date().getHours();
+    if (hour < 13 || (hour > 22 && hour <= 23)) return;
     if (!targetRoom.isStaff(targetUser)) return;
     const users: User[] = (targetRoom.users as string[]).map((u) => PS.getUser(Tools.toId(u)) as User);
     const Staffs = users.filter((u) => targetRoom.isStaff(u) && !targetRoom.isBot(u.id) && !u.isGlobalBot);
-    const isActiveStaffOnline = !Staffs.length || Staffs.some((e) => !(e.status ?? "").startsWith("!(Idle) "));
-    if (isActiveStaffOnline) return;
+    if (Staffs.length) {
+        const isActiveStaffOnline = Staffs.some((e) => !(e.status ?? "").startsWith("!(Idle) "));
+        if (isActiveStaffOnline) return;
+    }
     PS.sendRoom("japanese", "/modchat +");
 };
 
@@ -21,12 +24,12 @@ export const disableModchat = async (targetRoom: Room, targetUser: User) => {
     if (!targetRoom.modchat || targetRoom.modchat === "autoconfirmed") return;
     targetRoom = await PS.fetchRoom(targetRoom.id, false).catch(() => PS.getRoom(targetRoom.id)!);
 
-    if (new Date().getHours() > 13) return;
+    const hour = new Date().getHours();
+    if (hour > 13 && hour < 22) return;
     if (!targetRoom.isStaff(targetUser)) return;
     const users: User[] = (targetRoom.users as string[]).map((u) => PS.getUser(Tools.toId(u)) as User);
     const Staffs = users.filter((u) => targetRoom.isStaff(u) && !targetRoom.isBot(u.id) && !u.isGlobalBot);
-    const noActiveStaff = !Staffs.length || Staffs.every((e) => ["!(Idle) ", "!(Busy) "].includes((e.status ?? "").substring(8)));
-    if (noActiveStaff) return;
+    if (!Staffs.length) return;
     targetRoom.sendHTML({
         content: `<div class="infobox">Currently ModChat is set to "${targetRoom.modchat}". Do you want to set autoconfirmed?<br><button class="button" name="send" value="/msgroom japanese,/modchat ac">Set Modchat to autoconfirmed</button></div>`,
         id: "mcsuggestion",
