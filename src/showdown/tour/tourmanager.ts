96d8d7cb1232e1d6d249ed9e46bee4483aac3901
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-10-18 21:22:08 +0900

    Tournament: fix Crash

--- src/showdown/tour/tourmanager.ts
+++ src/showdown/tour/tourmanager.ts
@@ -1,9 +1,10 @@
 "use strict";
 
-import type { Room } from "@dirain/client";
+import type { Room, ClientUser } from "@dirain/client";
 
 export default async (room: Room, format: string) => {
-    if (!room.id.includes("japanese")) return;
+    if (!room.isStaff(PS.user as ClientUser)) return;
+    await Tools.sleep(PS.messageInterval);
     if (config.tourSettings.length > 5) {
         PS.sendArray(config.tourSettings.map((e) => `${room.id}|/tour ${e}`));
     } else {
@@ -12,7 +13,6 @@ export default async (room: Room, format: string) => {
     }
     const randomized = ["random", "factory", "hackmons", "staff"];
     if (randomized.some((e) => format.includes(e))) {
-        await Tools.sleep(PS.messageInterval);
         PS.sendRoom(room.id, "/tour scouting allow");
     }
 };
