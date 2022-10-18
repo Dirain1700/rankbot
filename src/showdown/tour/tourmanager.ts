ed6f195d919287692977d2507c21734ea0202f5c
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-10-18 14:47:10 +0000

    Tournament: Fix Bug

--- src/showdown/tour/tourmanager.ts
+++ src/showdown/tour/tourmanager.ts
@@ -3,7 +3,7 @@
 import type { Room, ClientUser } from "@dirain/client";
 
 export default async (room: Room, format: string) => {
-    if (!room.isStaff(PS.user as ClientUser)) return;
+    if (!room.isBot((PS.user as ClientUser).id) && !room.isStaff(PS.user as ClientUser)) return;
     await Tools.sleep(PS.messageInterval);
     if (config.tourSettings.length > 5) {
         PS.sendArray(config.tourSettings.map((e) => `${room.id}|/tour ${e}`));
