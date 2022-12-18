9329e3f7960829f9bc64aefb86fe1abe64bb8605
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-12-18 16:25:11 +0000

    Update config

--- config/config.ts
+++ config/config.ts
@@ -12,14 +12,13 @@ export const PSOptions: PSClientOptions = {
     pass: process.env.PS!,
     avatar: "elaine",
     status: "Hi! I am a bot! :)",
-    autoJoin: ["japanese"],
+    autoJoin: ["japanese", "botdevelopment"],
 };
 export const testCh: Snowflake = "1016979734988591104";
 export const logch: Snowflake = "922453739225374720";
 export const aptguild: Snowflake = "873211574876241931";
 export const owner: string = "dirain";
 export const admin: Snowflake[] = ["751433045529329825"];
-export const log: string[] = ["was banned", "was warned", "was muted", "was locked"];
 export const officials: string[] = ["japanese"];
 export const wordle: string[] = ["japanese"];
 export const tourSettings: string[] = ["autostart 5", "autodq 3"];
