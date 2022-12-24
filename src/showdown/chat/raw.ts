bdfcef3ca41b638b53af6c7aac69dad0ba92d61e
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-12-24 03:52:08 +0000

    Modchat: Fix typo

--- src/showdown/chat/raw.ts
+++ src/showdown/chat/raw.ts
@@ -4,7 +4,7 @@ import type { Room } from "@dirain/client";
 
 export default (message: string, room: Room): void => {
     const modchatRegex =
-        /<div class="broadcast-red"><strong>Moderated chat was set to (?<level>(off|autoconfirmed|trusted|\+|%|@|\*|player|#|&))!<\/strong><br \/>Only users of rank (off|autoconfirmed|trusted|\+|%|@|\*|player|#|&) and higher can talk.<\/div>/;
+        /<div class="broadcast-red"><strong>Moderated chat was set to (?<level>(autoconfirmed|trusted|\+|%|@|\*|player|#|&))!<\/strong><br \/>Only users of rank (autoconfirmed|trusted|\+|%|@|\*|player|#|&) and higher can talk.<\/div>/;
     if (!message.match(modchatRegex)) return;
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const { level } = message.match(modchatRegex)!.groups ?? {};
