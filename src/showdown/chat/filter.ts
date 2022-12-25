0011052a7b2d25b17a937e9ec636cf5425b8d1ea
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2022-12-25 11:42:27 +0000

    ChatFilter: Remove noMeaningFilter

--- src/showdown/chat/filter.ts
+++ src/showdown/chat/filter.ts
@@ -49,34 +49,6 @@ export default (message: Message<Room>): void => {
         return hasStretchSentence;
     };
 
-    const noMeaningFilter: (str: string) => boolean = (str: string) => {
-        if (str.length > 16) return false;
-        str = str.toLowerCase();
-        const strings: string[] = str.split(" ");
-        if (/\w/.test(str) && strings.length >= 5) return true;
-        // includes Hiragana
-        if (/[\u{3000}-\u{301C}\u{3041}-\u{3093}\u{309B}-\u{309E}]/mu.test(str)) return false;
-        // includes Katakana
-        if (/[\u{3000}-\u{301C}\u{30A1}-\u{30F6}\u{30FB}-\u{30FE}]/mu.test(str)) return false;
-        // includes Kanji
-        if (
-            /([\u{3005}\u{3007}\u{303b}\u{3400}-\u{9FFF}\u{F900}-\u{FAFF}\u{20000}-\u{2FFFF}][\u{E0100}-\u{E01EF}\u{FE00}-\u{FE02}]?)/mu.test(
-                str
-            )
-        )
-            return false;
-        if (strings.length >= 5) return true;
-        if (strings.length === 4 && str.length <= 8) return true;
-        if (strings.length === 3 && str.length <= 6) return true;
-        if (strings.length === 2 && str.length < 4) return true;
-        const set = new Set();
-        for (const strs of str.split("")) {
-            set.add(strs);
-        }
-        if (str.split("").length === set.size && str.length >= 13) return true;
-        return false;
-    };
-
     if (stretchFilter(message.content))
         return void (discord.channels.cache.get(config.testCh) as GuildTextBasedChannel)?.send?.({
             content: "Caught by stretchFilter: " + message.content,
