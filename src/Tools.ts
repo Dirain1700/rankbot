4d386506cae7ef803e41cdf6eb833005a0020686
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2023-03-29 14:45:28 +0000

    Tools: Improve `toString`

--- src/Tools.ts
+++ src/Tools.ts
@@ -1,5 +1,7 @@
 "use strict";
 
+import { inspect } from "node:util";
+
 import { Tools as PSTools } from "@dirain/client";
 
 export class Tools extends PSTools {
@@ -42,36 +44,7 @@ export class Tools extends PSTools {
                     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                     return "[" + input.map((e) => this.toString(e, depth! + 1)).join(", ") + "]";
                 }
-                if (depth > 0) {
-                    if (input.constructor?.name && input.constructor.name !== "Object") {
-                        return "[class " + input.constructor.name + "]";
-                    } else if (Reflect.ownKeys(JSON).every((key) => key in input)) {
-                        return "[object JSON]";
-                    } else if (Reflect.ownKeys(Math).every((key) => key in input)) {
-                        return "[object Math]";
-                    } else if (Reflect.ownKeys(Atomics).every((key) => key in input)) {
-                        return "[object Atomics]";
-                    } else if (Reflect.ownKeys(Reflect).every((key) => key in input)) {
-                        return "[object Reflect]";
-                    } else if (Reflect.ownKeys(Intl).every((key) => key in input)) {
-                        return "[object Intl]";
-                    } else if (Reflect.ownKeys(WebAssembly).every((key) => key in input)) {
-                        return "[object WebAssembly]";
-                    } else if (depth > 2) {
-                        return "[object Object]";
-                    }
-                }
-                const props: [string, string][] = [];
-                for (const [k, v] of Object.entries(input)) {
-                    if (k.toString().startsWith(input.constructor?.name || "constructor")) continue;
-                    props.push([this.toString(k, depth + 1), this.toString(v, depth + 1)]);
-                }
-                const stringProps = props.map(([k, v]) => k + ": " + v).join(", ");
-                if (input.constructor?.name && input.constructor.name !== "Object") {
-                    if (stringProps.length) return "{ " + stringProps + " }";
-                    else return input.constructor.name + " {}";
-                } else if (stringProps.length) return "{ " + stringProps + " }";
-                else return "{}";
+                return Tools.trim(inspect(input, { depth: depth ?? 1 }).replaceAll("\n", ""));
             }
             default:
                 // eslint-disable-next-line @typescript-eslint/no-explicit-any
