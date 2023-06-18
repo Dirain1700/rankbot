9cdf4a8f4c76683e1f7647c6c8fc9083d69c2d0e
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2023-06-18 05:32:37 +0000

    Modchat: Improve settings to showing rfaq

--- src/showdown/chat/modchat/detect.ts
+++ src/showdown/chat/modchat/detect.ts
@@ -8,7 +8,12 @@ export default (targetRoom: Room, currentModchatLevel: ModchatLevel, previousMod
     if (!Config.modchatTime[targetRoom.roomid]) return;
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const { startTime, endTime, always, rank, showRfaq } = Config.modchatTime[targetRoom.roomid]!;
-    if (showRfaq && currentModchatLevel === rank && checkCondition(startTime, endTime, !!always, new Date().getHours()) && Tools.isHigherAuth(currentModchatLevel, previousModchatLevel)) {
+    if (
+        showRfaq &&
+        currentModchatLevel === rank &&
+        checkCondition(startTime, endTime, !!always, new Date().getHours()) &&
+        Tools.isHigherAuth(currentModchatLevel, previousModchatLevel)
+    ) {
         targetRoom.send("!rfaq modchat");
     }
 };
