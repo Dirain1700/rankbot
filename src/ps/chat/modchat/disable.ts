f18c4f6dad553a63dbc33b685541ac7673ca11e1
Dirain1700 <86363603+Dirain1700@users.noreply.github.com>
2025-11-14 17:28:34 +0900

    Modchat: Strict conditions on auto disable

--- src/ps/chat/modchat/disable.ts
+++ src/ps/chat/modchat/disable.ts
@@ -5,16 +5,17 @@ import { checkTimeCondition } from "./enable";
 import type { Room } from "../../client/src";
 
 export function disableModchat(targetRoom: Room): boolean {
-    if (!BotClient.ps.user || !targetRoom.checkCan("warn", BotClient.ps.user, false)) {
+    if (!BotClient.ps.user) {
         return false;
     }
     if (!Config.roomSettings[targetRoom.roomid]?.modchat?.autoReset) return false;
 
     const currentModchatLevel = targetRoom.update().modchat;
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const { startTime, endTime, always, baseRank } = Config.roomSettings[targetRoom.roomid]!.modchat!;
+    const { startTime, endTime, always, baseRank, elevatedRank } = Config.roomSettings[targetRoom.roomid]!.modchat!;
     if (
         Tools.isHigherAuth(currentModchatLevel, baseRank, true) &&
+        !Tools.isHigherAuth(currentModchatLevel, elevatedRank || "+", true) &&
         !checkTimeCondition(startTime, endTime, !!always, new Date().getHours())
     ) {
         targetRoom.setModchat(baseRank);
